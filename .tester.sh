188c2e419a3a417d07dd2613bad145fe683121c6
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-07-04 06:47:57 -0300

    Update .tester.sh

--- .tester.sh
+++ .tester.sh
@@ -7,13 +7,20 @@ echo "Arquivo invisiveis encontrados:"
 find . -name ".*" | sort -n
 echo "------------------------------------------------------------"
 echo "Resultado do <main>"
-if ([ -e ./*.c ]); then
-	cat ./*.c | sed -e 's/\/\*.*//g' | sed -e 's/\*\///g' > /tmp/tmp.c 
-	clang -Wall -Wextra -Werror /tmp/tmp.c
-	./a.out
-	rm a.out
+if ([ $1 ]); then
+                EXECUTABLE=$1;
+                ARCHIVES=`ls -F *.c | sed -e "s/$1/\/tmp\/tmp.c/g"`;
+        else
+                EXECUTABLE='*.c';
+                ARCHIVES='/tmp/tmp.c';
+fi;
+if ([ -e $EXECUTABLE ]); then
+        cat $EXECUTABLE | sed -e 's/\/\*.*//g' | sed -e 's/\*\///g' > /tmp/tmp.c;
+        clang -Wall -Wextra $ARCHIVES -lm;
+        ./a.out $2 $3;
+        rm a.out;
 else
-	echo "Sem arquivos para compilar"
-fi
+        echo "Sem arquivos para compilar"
+fi;
 echo ""
 echo "------------------------------------------------------------"
