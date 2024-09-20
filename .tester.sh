548d86d125797ac442a7dd9ddee80e55569f1615
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-09-20 19:43:26 -0300

    Update .tester.sh

--- .tester.sh
+++ .tester.sh
@@ -4,21 +4,21 @@ echo "Verificação Norminette:"
 norminette | sort -n
 echo "------------------------------------------------------------" 
 echo "Arquivo invisiveis encontrados:" 
-find . -name "." | sort -n
+find . -name ".*" | sort -n
 echo "------------------------------------------------------------"
 echo "Executaveis encontrados:"
-find . -name ".out" | sort -n
+find . -name "*.out" | sort -n
 echo "------------------------------------------------------------"
 echo "Resultado do <main>"
 if ([ $1 ]); then
                 EXECUTABLE=$1;
-                ARCHIVES=ls -F *.c | sed -e "s/$1/\/tmp\/tmp.c/g";
+                ARCHIVES=`ls -F *.c | sed -e "s/$1/\/tmp\/tmp.c/g"`;
         else
-                EXECUTABLE='.c';
+                EXECUTABLE='*.c';
                 ARCHIVES='/tmp/tmp.c';
 fi;
 if ([ -e $EXECUTABLE ]); then
-        cat $EXECUTABLE | sed -e 's//*.//g' | sed -e 's/*///g' > /tmp/tmp.c;
+        cat $EXECUTABLE | sed -e 's/\/\*.*//g' | sed -e 's/\*\///g' > /tmp/tmp.c;
         clang -Wall -Wextra $ARCHIVES -lm -lbsd -g;
         ./a.out $2 $3 $4 $6;
 else
