9d2784f506dfa17f6d7b448beee9b89ecd0b2fa6
Victor Hugo <83098581+victorhdsp@users.noreply.github.com>
2024-09-23 09:26:41 -0300

    Create tester_make.sh

---
+++ tester_make.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd __test__
+
+COMPILADOR="gcc";
+HEADER_FOLDER="../src/headers/"; #Relativo a pasta de testes.
+C_FOLDER="../src/src"; #Relativo a pasta de testes.
+TEST_SRC="./src"
+
+ALL="unity.o main.o $(ls "$C_FOLDER" | tr "\n" " " | sed -e 's|\.c|\.o|g')"
+
+(
+    echo "test: $ALL";
+    echo "	$COMPILADOR -o test $ALL";
+    echo "";
+    echo "unity.o: $TEST_SRC/unity.c";
+    echo -e "\t$COMPILADOR -c $TEST_SRC/unity.c -o unity.o";
+    echo "";
+    echo "main.o: main.c";
+    echo -e "\t$COMPILADOR -I $TEST_SRC -I $HEADER_FOLDER -c main.c -o main.o";
+    echo "";
+    for C_FILE in $(ls "$C_FOLDER/"); do
+        O_FILE=$(echo "$C_FILE" | sed -e 's|\.c|\.o|g')
+        echo -e "$O_FILE: $C_FOLDER/$C_FILE";
+        echo -e "\t$COMPILADOR -I $HEADER_FOLDER -c $C_FOLDER/$C_FILE -o $O_FILE";
+    done
+    echo "";
+    echo "clean:";
+    echo "	rm -f *.o test";
+) > makefile
+
+make
+./test
